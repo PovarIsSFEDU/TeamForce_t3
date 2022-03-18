a0423dfa79d69775e4469b226ebfdecf410217e1
Юров Ярослав Евгеньевич <yurov_ye@rubin.local>
2022-03-18 23:59:38 +0300

    Добавил удаление записи

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,10 @@
-import logging
 import os
 from sqlalchemy import select
 import sys
 import telebot
 from keyboa import Keyboa
 import logging
-from business import insert, select_all, db_session, User, Topic, Message, init_migrate
+from business import insert, select_all, db_session, Users, Topic, Message, init_migrate, select_max_id, delete
 
 init_migrate()
 TOKEN = os.environ.get("TOKEN")
@@ -19,16 +18,30 @@ teams = db_session.execute(select(Topic))
 teams_count = sum(1 for _ in teams)
 
 
+def get_user_id(username):
+    res = select_all(Users, Users.username == username)
+    return res[0].get("id") if res else None
+
+
 @bot.message_handler(commands=['start', 'help'])
 def start_bot(message):
     menu = ["Создать тэг/команду", "Список чатов", "Помощь", "О создателях"]
     keyboard = Keyboa(items=menu)
+    msg_json = message.json
+    username = msg_json["from"].get("username")
+    first_name = msg_json["from"].get("first_name")
+    last_name = msg_json["from"].get("last_name")
+    if get_user_id(username) is None:
+        id_ = select_max_id(Users)
+        id_ = id_ if id_ is not None else 0
+        insert(Users, user_id=id_ + 1, first_name=first_name, last_name=last_name, username=username, admin=False,
+               phone="")
     bot.send_message(chat_id=message.chat.id,
                      text="Добро пожаловать! Пожалуйста, выберите команду! <TODO: сделать входной текст>",
                      reply_markup=keyboard())
 
 
-#Расположение клавиатуры для всех созданных команд
+# Расположение клавиатуры для всех созданных команд
 @bot.message_handler(commands=['teams', 'tags'])
 def all_topics(message):
     if (teams_count == 0):
@@ -42,28 +55,28 @@ def all_topics(message):
     bot.send_message(chat_id=message.chat.id, text=text, reply_markup=keyboard())
 
 
-#Расположение клавиатуры для одной команды
+# Расположение клавиатуры для одной команды
 @bot.message_handler(commands=['check_team'])
 def exact_topic(message):
-    menu = ["Просмотреть сообщения", "Получить список пользователей", "Создать рассылку", "Получить ссылку", "Удалить команду/тэг", "Назад"]
+    menu = ["Просмотреть сообщения", "Получить список пользователей", "Создать рассылку", "Получить ссылку",
+            "Удалить команду/тэг", "Назад"]
     keyboard = Keyboa(items=menu)
     text = "Вы выбрали тестовый тэг/команду для просмотра."
     bot.send_message(chat_id=message.chat.id, text=text, reply_markup=keyboard())
 
 
-#@bot.message_handler(commands=['team', 'tag'])
-#def topic_creation(message):
+# @bot.message_handler(commands=['team', 'tag'])
+# def topic_creation(message):
 #
 #   bot.reply_to(message, "Команда(тэг) создана. А вот и ссылка для присоединения: " + "start=")
 
 
+# Начинается User-Side
 
-#Начинается User-Side
-
-#Расположение клавиатуры для одной команды
+# Расположение клавиатуры для одной команды
 @bot.message_handler(commands=['send'])
 def prepare_send_to_topic(message):
-    teams = db_session.execute(select(Topic).join(User).filter(User.id == message.from_user.id)) #TODO Join-query
+    teams = db_session.execute(select(Topic).join(Users).filter(Users.id == message.from_user.id))  # TODO Join-query
     teams_count = sum(1 for _ in teams)
     if (teams_count == 0):
         menu = ["Выбрать команды/тэги", "Назад"]
@@ -72,10 +85,7 @@ def prepare_send_to_topic(message):
         menu = [x.name for x in teams] + ["Назад"]
         text = "Выберите тему/команду/тэг в которую хотите написать"
     keyboard = Keyboa(items=menu)
-    text = "После этого вы напишете тестовое сообщение."
     bot.send_message(chat_id=message.chat.id, text=text, reply_markup=keyboard())
-
-
 
 
 @bot.message_handler(func=lambda m: True)
