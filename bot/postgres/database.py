e1514911ef9507b11e0e60087ecd84d3e61726b2
Юров Ярослав Евгеньевич <yurov_ye@rubin.local>
2022-03-18 18:45:18 +0300

    Добавил модели

--- bot/postgres/database.py
+++ bot/postgres/database.py
@@ -2,7 +2,7 @@
 
 
 import os
-from sqlalchemy import create_engine, Column, Integer, String, Text, Date
+from sqlalchemy import create_engine, Column, Integer, String, Text, Date, Table, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -17,19 +17,51 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
-
 Model = declarative_base(name='Model')
 
 
 class Employ(Model):
     __tablename__ = 'employ'
-    id = Column('employ_id', Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(200))
     project_name = Column(String(200))
     message = Column(Text)
     date_message = Column(Date)
 
-    def __init__(self, employ_id,  name, project_name, date_message):
+    def __init__(self, employ_id, name, project_name, date_message):
+        self.id = employ_id
+        self.name = name
+        self.project_name = project_name
+        self.date_message = date_message
+
+
+class Theme(Model):
+    __tablename__ = 'theme'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(500))
+
+    def __init__(self, theme_id, name):
+        self.id = theme_id
+        self.name = name
+
+
+employ_theme = Table('employ_theme', Model.metadata,
+                     Column('employ_id', ForeignKey('employ.id')),
+                     Column('theme_id', ForeignKey('theme.id'))
+                     )
+
+
+class Message(Model):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    theme_id = Column(Integer, ForeignKey('theme.id'))
+    employ_id = Column(Integer, ForeignKey('employ.id'))
+    status = Column(String(50))
+    type = Column(String(50))
+    message_text = Column(Text)
+    date = Column(Date)
+
+    def __init__(self, employ_id, name, project_name, date_message):
         self.id = employ_id
         self.name = name
         self.project_name = project_name
@@ -38,5 +70,3 @@ class Employ(Model):
 
 def init_migrate():
     Model.metadata.create_all(engine)
-
-
